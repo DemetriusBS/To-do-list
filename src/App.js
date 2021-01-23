12cdbd7027f7a47c562beda4803df453c4c33ed6
DemetriusBS <dbsfox@outlook.com>
2021-01-23 17:06:01 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: ''
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='addTask'>
+          <fieldset className='fieldsetTask'>
+            <legend>Informe as suas tarefas</legend>
+            <label for='input-task'>Tarefa: </label>
+            <input id='input-task' value={this.state.task}
+              onChange={(e) => this.setState({ task: e.target.value })}
+            />
+          </fieldset>
+
+          <button className='buttonTask'>Salvar</button>
+        </div>
+
+        <h2>
+          To do:
+        </h2>
+      </div>
+    )
+  }
 }
 
 export default App;
